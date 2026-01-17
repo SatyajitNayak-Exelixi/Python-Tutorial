2908ffb43d8b1d87651503c955b9a789e1c54d8b
Satyajit Nayak <satyajit.nayak@exelixi.io>
2026-01-17 11:49:49 +0530

    initial commit

---
+++ Chapter-1/PraticeSet-1/ps-1.py
@@ -0,0 +1,10 @@
+# Write a program to print a poetry.
+
+poem = """
+Whispers of wind in the silent night,
+Stars paint dreams with gentle light.
+Hope blooms soft where shadows stay,
+A new tomorrow starts today.
+"""
+
+print(poem)
---
+++ Chapter-1/PraticeSet-1/ps-2.py
@@ -0,0 +1,8 @@
+# Install a external module and use it. 
+
+
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("Hello Satyajit, pyttsx3 is working fine")
+engine.runAndWait()
